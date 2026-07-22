76a1481d9d5cbfcebbae0ad37f8c77e758a593e5
LNMaleGran <kirill.malcev.omsk@gmail.com>
2026-07-22 16:21:42 +0600

    мольба

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
-        "https://your-app.vercel.app",  # ← ДОБАВЬ ПОСЛЕ ДЕПЛОЯ
+        "https://gelios-tau.vercel.app",  # ← ДОБАВЬ ПОСЛЕ ДЕПЛОЯ
     ],
     allow_credentials=True,
     allow_methods=["*"],
